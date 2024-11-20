c06b42ec26109c30e95ac2e1ff7b0152d752d713
herdavid333 <herva61@gmail.com>
2024-11-20 17:20:50 -0500

    config .bat

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,5 @@ kubectl apply -f test-consumer/consumer_deployment.yaml
 kubectl apply -f santiago-greetings/santiago_greetings_deployment.yaml
 kubectl apply -f lenin-greetings/lenin_greetings_deployment.yaml
 kubectl apply -f kelly-greetings/kelly_greetings_deployment.yaml
+kubectl apply -f Hernan-app/hernan_greetings_deployment.yaml
 echo "All Deployed!"
