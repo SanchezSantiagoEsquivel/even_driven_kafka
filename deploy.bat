7156ac96c226cb712b000354aa9b4b1d261b9533
luisgarogu <luis.gabriel.rodriguez@correounivalle.edu.co>
2024-11-19 23:27:55 -0500

    Nuevos Cambios

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,5 @@ kubectl apply -f zookeeper.yaml
 kubectl apply -f kafka-broker.yaml
 kubectl apply -f test-consumer/consumer_deployment.yaml
 kubectl apply -f santiago-greetings/santiago_greetings_deployment.yaml
+kubectl apply -f luis-greetings/luis_greetings_deployment.yaml
 echo "All Deployed!"
